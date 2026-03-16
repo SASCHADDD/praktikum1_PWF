b2174c6953b39597a68f54ed4313b1d350a493f6
SASCHADDD <saschadanu23@gmail.com>
2026-03-17 00:01:45 +0700

    mendefinisikan route setiap crud

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,13 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+    Route::post('/products', [ProductController::class, 'store'])->name('products.store');
+    Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
+    Route::get('/products/{id}', [ProductController::class, 'show'])->name('products.show');
+    Route::put('/products/{id}', [ProductController::class, 'update'])->name('products.update');
+    Route::delete('/products/{id}', [ProductController::class, 'delete'])->name('products.delete');
 });
 
 require __DIR__.'/auth.php';
